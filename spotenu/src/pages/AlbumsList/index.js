382b39a0957f7f4af192131d225c52f6a2c83ea4
Géssica Costa de Araújo Souza <gcas_@hotmail.com>
2020-09-16 22:19:03 -0300

    Correção do useRequestPost.

--- spotenu/src/pages/AlbumsList/index.js
+++ spotenu/src/pages/AlbumsList/index.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import { useHistory } from 'react-router-dom';
 import HeaderBand from '../../components/HeaderBand';
 import { useLateralMenu } from '../../hooks/useLateralMenu';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
 
 const Wrapper = styled.div`
     width: 100%;
@@ -17,6 +18,23 @@ const Content = styled.div`
     flex-grow: 1;
 `;
 
+const Principal = styled.div`
+    flex-grow: 1;
+    padding: 56px;
+`;
+
+const Title = styled.div`
+    flex-grow: 1;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+`;
+
+const H1 = styled.h1`
+    font-size: 48px;
+    margin-right: 32px;
+`;
+
 function AlbumList() {
     const history = useHistory();
     const { conditionalLateralMenu, openLateralMenu } = useLateralMenu("BAND");
@@ -26,6 +44,12 @@ function AlbumList() {
             <HeaderBand openLateralMenu={openLateralMenu} />
             <Content>
                 {conditionalLateralMenu}
+                <Principal>
+                    <Title>
+                        <H1>Álbuns</H1>
+                        <AddCircleIcon fontSize="large" />
+                    </Title>
+                </Principal>
             </Content>
         </Wrapper>
     );
